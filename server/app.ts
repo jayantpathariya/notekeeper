8ea49dc08323d18d5db77b5826144a88c6730c83
Jayant Pathariya <jayantmehra02@gmail.com>
2024-05-15 22:30:10 +0530

    client added as static file

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,25 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
 import auth from "./routes/auth";
 import notebooks from "./routes/notebooks";
 import notes from "./routes/notes";
 
-const app = new Hono().basePath("/api");
+const app = new Hono();
 
 app.use(logger());
+app.use(
+  "/*",
+  serveStatic({
+    root: "./public",
+  })
+);
 
-app.route("/auth", auth).route("/notebooks", notebooks).route("/notes", notes);
+app
+  .basePath("/api")
+  .route("/auth", auth)
+  .route("/notebooks", notebooks)
+  .route("/notes", notes);
 
 export default app;
